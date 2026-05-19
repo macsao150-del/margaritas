5a2cc843c3c441fbd8cefa4d8c9c90fd4d3171ce
macsao150-del <macsao150@gmail.com>
2026-05-19 12:23:18 -0700

    Ajustes en la vista index y en la sincronizacion del audio

--- anim.js
+++ anim.js
@@ -4,56 +4,64 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "Even if my heart stops beating", time: 19, duration: 4 },
+  { text: "Even if my heart stops beating", time: 22, duration: 4 },
   {
+    //19
     text: "You're the only thing I need, ooh, with me",
-    time: 20,
+    time: 23, //20
     duration: 10,
   },
-  { text: "Even if the Earth starts shaking", time: 32, duration: 4 },
+  { text: "Even if the Earth starts shaking", time: 34, duration: 4 },
   {
+    //31
     text: "You're the only thing worth taking, ooh, with me",
-    time: 31,
+    time: 35, //32
     duration: 11,
   },
-  { text: "Even if the sky's on fire", time: 44, duration: 4 },
-  { text: "Got you here, it's alright, ooh, with me", time: 45, duration: 10 },
-  { text: "And if it's all over", time: 56.1, duration: 3.8 },
-  { text: "I'm taking this moment, ooh, with me", time: 58.8, duration: 8.9 },
-
-  { text: "Pretty boy, you did this with me, boy", time: 83.8, duration: 6.8 },
-  { text: "Now it's all about to end", time: 91.6, duration: 3.9 },
-  { text: "Baby boy, look where we made it, boy", time: 96, duration: 7 },
-  { text: "Hmm, now we're falling", time: 103, duration: 4 },
-
-  { text: "As long as I got you", time: 106, duration: 5 },
-  { text: "I'm gonna be alright", time: 109, duration: 7 },
-  { text: "As long as I got you, yeah", time: 118, duration: 5 },
-  { text: "I'm not afraid to die", time: 122.9, duration: 5 },
-  { text: "I'm alright'", time: 128, duration: 3 },
-
-  { text: "Even if my heart stops beating", time: 131, duration: 3.9 },
+  { text: "Even if the sky's on fire", time: 47, duration: 4 }, //44
+  { text: "Got you here, it's alright, ooh, with me", time: 48, duration: 10 }, //45
+  { text: "And if it's all over", time: 59.1, duration: 3.8 }, //56.1
+  { text: "I'm taking this moment, ooh, with me", time: 61.5, duration: 8.9 }, //58.8
+  //listo
+  { text: "Pretty boy, you did this with me, boy", time: 86.5, duration: 6.8 }, //83.8
+  { text: "Now it's all about to end", time: 94.3, duration: 3.9 }, //91.6
+  { text: "Baby boy, look where we made it, boy", time: 98.8, duration: 7 }, //96
+  { text: "Hmm, now we're falling", time: 105.7, duration: 4 }, //103
+  //listo
+  { text: "As long as I got you", time: 108.6, duration: 5 }, //106
+  { text: "I'm gonna be alright", time: 111.7, duration: 7 }, //109
+  //listo
+  { text: "As long as I got you, yeah", time: 120.8, duration: 5 }, //118
+  { text: "I'm not afraid to die", time: 125.6, duration: 5 }, //122.9
+  { text: "I'm alright'", time: 131, duration: 3 }, //128
+  //listo
+  { text: "Even if my heart stops beating", time: 133.7, duration: 4 }, //131
   {
-    text: "You're the only thing I need, ooh, with me",
-    time: 134,
+    text: "You're the only thing I need, ooh, with me", //134
+    time: 136.6,
     duration: 10,
   },
-  { text: "Even if the Earth starts shaking", time: 143, duration: 4 },
+  //listo
+  { text: "Even if the Earth starts shaking", time: 145.6, duration: 4 }, //143
   {
     text: "You're the only thing worth taking, ooh, with me",
-    time: 146.1,
+    time: 150.4, //146.7
     duration: 10.6,
   },
-  { text: "Even if the sky's on fire", time: 154.8, duration: 4 },
-  { text: "Got you here, it's alright, ooh, with me", time: 156, duration: 10 },
-  { text: "And if it's all over", time: 168, duration: 4 },
-  { text: "I'm taking this moment, ooh, with me", time: 169, duration: 9.5 },
+  { text: "Even if the sky's on fire", time: 158.4, duration: 4 }, //154.8
+  {
+    text: "Got you here, it's alright, ooh, with me",
+    time: 159.6,
+    duration: 10,
+  }, //156
+  { text: "And if it's all over", time: 171.5, duration: 4 }, //168
+  { text: "I'm taking this moment, ooh, with me", time: 172.5, duration: 9.5 }, //169
 
-  { text: "Wherever I'm going", time: 180, duration: 4 },
-  { text: "I'm happy you're coming, ooh, with me", time: 183, duration: 8.3 },
-  { text: "With me, yeah", time: 191, duration: 4 },
+  { text: "Wherever I'm going", time: 183.6, duration: 4 }, //180
+  { text: "I'm happy you're coming, ooh, with me", time: 186.5, duration: 8.3 }, //183
+  { text: "With me, yeah", time: 194.5, duration: 4 }, //191
 
-  { text: "No, I'm lucky you're with me", time: 202, duration: 5 },
+  { text: "No, I'm lucky you're with me", time: 205.5, duration: 5 }, //202
 ];
 
 // Animar las letras
